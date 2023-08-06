98a717d8da43d812e94749541b26c6dd0732f75d
Andrew Liebchen <andrewliebchen@gmail.com>
2023-08-06 16:47:52 -0700

    Stub in content

--- src/App.js
+++ src/App.js
@@ -1,7 +1,89 @@
 function App() {
   return (
     <div>
-      <h1>Hi</h1>
+      <div>
+        <h1>Full-scale Impact, For A Fraction of the Time.</h1>
+      </div>
+      <div>
+        <h2>Say hello to your first designer</h2>
+        <p>
+          The early stages of a start-up are exciting and challenging. With an
+          abundance of ideas and a paucity of resources, you need team members
+          who can wear multiple hats, deliver high-quality results, and add
+          value beyond their immediate role.
+        </p>
+      </div>
+      <div>
+        <h2>I bring ideas to life</h2>
+        <p>
+          As your fractional product designer, I plug seamlessly into your team
+          to drive the product development journey.
+        </p>
+        <div>
+          <div>
+            <h4>Concept development</h4>
+            <p>
+              I'll work closely with your product team to crystallize your ideas
+              into achievable visions and actionable plans.
+            </p>
+          </div>
+        </div>
+      </div>
+      <div>
+        <p>
+          With over a decade of experience in crafting consumer, enterprise, and
+          data-driven product experiences, from pre-money start-ups to giants
+          like Meta, I bring the insight and expertise of a full-time product
+          designer to your project - on a contractor basis.
+        </p>
+        <h3>Equity for Excellence</h3>
+        <p>
+          I am committed to your start-up's success. As part of my unique
+          approach, I accept equity in lieu of full-time salary. This ensures
+          our goals are aligned, and I am as invested in your success as you
+          are.
+        </p>
+        <h3>Working with me</h3>
+        <p>
+          I love solving hard problems and relish the chance to roll up my
+          sleeves with good people. As a full-stack product designer, I am
+          pragmatic, detail-oriented, and bring an open heart and mind to
+          everything I do.
+        </p>
+      </div>
+      <div>
+        <h2>Case studies</h2>
+        <p>
+          Ready to make your start-up's product vision a reality? Reach out and
+          let's make great things happen, together.
+        </p>
+        <div>
+          <div>
+            <h3>Meta Quest App</h3>
+            <p>
+              Within the dynamic environment of Meta, I spearheaded the redesign
+              of the Quest VR headset's companion app. The focus? Simplifying
+              the complex and addressing the core issues faced by users.
+            </p>
+          </div>
+          <div>
+            <h3>Watch Duty</h3>
+            <p>
+              Within the dynamic environment of Meta, I spearheaded the redesign
+              of the Quest VR headset's companion app. The focus? Simplifying
+              the complex and addressing the core issues faced by users.
+            </p>
+          </div>
+        </div>
+      </div>
+      <div>
+        <h2>Ready to get started?</h2>
+        <p>
+          Hit me up on email:
+          <br />
+          <a href="mailto:andrew@liebchen.world">andrew@liebchen.world</a>
+        </p>
+      </div>
     </div>
   );
 }
