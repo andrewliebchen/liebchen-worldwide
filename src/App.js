470d148e2842b85437f980d575a2ea7e72e233fc
Andrew Liebchen <andrewliebchen@gmail.com>
2023-08-06 17:02:15 -0700

    Add theme UI

--- src/App.js
+++ src/App.js
@@ -1,90 +1,98 @@
+import theme from "./theme";
+import { ThemeUIProvider, Box, Text, Heading } from "theme-ui";
+
 function App() {
   return (
-    <div>
-      <div>
-        <h1>Full-scale Impact, For A Fraction of the Time.</h1>
-      </div>
-      <div>
-        <h2>Say hello to your first designer</h2>
-        <p>
+    <ThemeUIProvider theme={theme}>
+      <Box>
+        <Heading as="h1">
+          Full-scale Impact, For A Fraction of the Time.
+        </Heading>
+      </Box>
+      <Box>
+        <Heading as="h2">Say hello to your first designer</Heading>
+        <Text>
           The early stages of a start-up are exciting and challenging. With an
           abundance of ideas and a paucity of resources, you need team members
           who can wear multiple hats, deliver high-quality results, and add
           value beyond their immediate role.
-        </p>
-      </div>
-      <div>
-        <h2>I bring ideas to life</h2>
-        <p>
+        </Text>
+        >
+      </Box>
+      <Box>
+        <Heading as="h2">I bring ideas to life</Heading>
+        <Text>
           As your fractional product designer, I plug seamlessly into your team
           to drive the product development journey.
-        </p>
-        <div>
-          <div>
-            <h4>Concept development</h4>
-            <p>
+        </Text>
+        >
+        <Box>
+          <Box>
+            <Heading as="h4">Concept development</Heading>
+            <Text>
               I'll work closely with your product team to crystallize your ideas
               into achievable visions and actionable plans.
-            </p>
-          </div>
-        </div>
-      </div>
-      <div>
-        <p>
+            </Text>
+          </Box>
+        </Box>
+      </Box>
+      <Box>
+        <Text>
           With over a decade of experience in crafting consumer, enterprise, and
           data-driven product experiences, from pre-money start-ups to giants
           like Meta, I bring the insight and expertise of a full-time product
           designer to your project - on a contractor basis.
-        </p>
-        <h3>Equity for Excellence</h3>
-        <p>
+        </Text>
+        <Heading as="h3">Equity for Excellence</Heading>
+        <Text>
           I am committed to your start-up's success. As part of my unique
           approach, I accept equity in lieu of full-time salary. This ensures
           our goals are aligned, and I am as invested in your success as you
           are.
-        </p>
-        <h3>Working with me</h3>
-        <p>
+        </Text>
+        <Heading as="h3">Working with me</Heading>
+        <Text>
           I love solving hard problems and relish the chance to roll up my
           sleeves with good people. As a full-stack product designer, I am
           pragmatic, detail-oriented, and bring an open heart and mind to
           everything I do.
-        </p>
-      </div>
-      <div>
-        <h2>Case studies</h2>
-        <p>
+        </Text>
+      </Box>
+      <Box>
+        <Heading as="h2">Case studies</Heading>
+        <Text>
           Ready to make your start-up's product vision a reality? Reach out and
           let's make great things happen, together.
-        </p>
-        <div>
-          <div>
-            <h3>Meta Quest App</h3>
-            <p>
+        </Text>
+        <Box>
+          <Box>
+            <Heading as="h3">Meta Quest App</Heading>
+            <Text>
               Within the dynamic environment of Meta, I spearheaded the redesign
               of the Quest VR headset's companion app. The focus? Simplifying
               the complex and addressing the core issues faced by users.
-            </p>
-          </div>
-          <div>
-            <h3>Watch Duty</h3>
-            <p>
+            </Text>
+          </Box>
+          <Box>
+            <Heading as="h3">Watch Duty</Heading>
+            <Text>
               Within the dynamic environment of Meta, I spearheaded the redesign
               of the Quest VR headset's companion app. The focus? Simplifying
               the complex and addressing the core issues faced by users.
-            </p>
-          </div>
-        </div>
-      </div>
-      <div>
-        <h2>Ready to get started?</h2>
-        <p>
+            </Text>
+            >
+          </Box>
+        </Box>
+      </Box>
+      <Box>
+        <Heading as="h2">Ready to get started?</Heading>
+        <Text>
           Hit me up on email:
           <br />
           <a href="mailto:andrew@liebchen.world">andrew@liebchen.world</a>
-        </p>
-      </div>
-    </div>
+        </Text>
+      </Box>
+    </ThemeUIProvider>
   );
 }
 
