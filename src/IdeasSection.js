0cd38778e74c74b1bbf9130389a2e2a6f54be082
Andrew Liebchen <andrewliebchen@gmail.com>
2023-08-06 19:48:02 -0700

    IdeasSection pagination

--- src/IdeasSection.js
+++ src/IdeasSection.js
@@ -1,4 +1,5 @@
-import { Box, Text, Heading } from "theme-ui";
+import { Box, Text, Heading, Flex, Grid } from "theme-ui";
+import { useState } from "react";
 
 const slides = [
   {
@@ -16,22 +17,31 @@ const slides = [
 ];
 
 function IdeasSection() {
+  const [counter, setCounter] = useState(0);
+
+  const handleNextClick = () => {
+    if (slides.length - 1 === counter) {
+      setCounter(0);
+    } else {
+      setCounter(counter + 1);
+    }
+  };
+
   return (
-    <Box p={6}>
+    <Flex p={6} sx={{ gap: 3, flexDirection: "column" }}>
       <Heading as="h2">I bring ideas to life</Heading>
       <Text>
         As your fractional product designer, I plug seamlessly into your team to
         drive the product development journey.
       </Text>
-      <Box>
-        {slides.map((slide, i) => (
-          <Box index={i}>
-            <Heading as="h4">{slide.heading}</Heading>
-            <Text>{slide.text}</Text>
-          </Box>
-        ))}
-      </Box>
-    </Box>
+      <Grid columns="1fr 1fr" gap={3}>
+        <Box p={3}>
+          <Heading as="h4">{slides[counter].heading}</Heading>
+          <Text>{slides[counter].text}</Text>
+        </Box>
+      </Grid>
+      <a onClick={() => handleNextClick()}>Next</a>
+    </Flex>
   );
 }
 
