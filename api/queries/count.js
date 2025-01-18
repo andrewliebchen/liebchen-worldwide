515b269aba0a1f8b704806cdf8179b4209d545f6
Andrew Liebchen <andrewliebchen@gmail.com>
2025-01-18 12:37:33 -0800

    Fix

--- api/queries/count.js
+++ api/queries/count.js
@@ -1,20 +1,15 @@
 import { withIronSessionApiRoute } from 'iron-session/next';
-
-const sessionOptions = {
-  password: process.env.SESSION_SECRET,
-  cookieName: 'andrew_ai_session',
-  cookieOptions: {
-    secure: process.env.NODE_ENV === 'production',
-    maxAge: 24 * 60 * 60 // 24 hours
-  }
-};
+import { SESSION_CONFIG, isAIEnabled } from '../../config/openai';
 
 async function countRoute(req, res) {
   if (req.method !== 'GET') {
     return res.status(405).json({ error: 'Method not allowed' });
   }
 
-  res.json({ queryCount: req.session.queryCount || 0 });
+  res.json({ 
+    queryCount: req.session.queryCount || 0,
+    aiEnabled: isAIEnabled()
+  });
 }
 
-export default withIronSessionApiRoute(countRoute, sessionOptions); 
+export default withIronSessionApiRoute(countRoute, SESSION_CONFIG); 
